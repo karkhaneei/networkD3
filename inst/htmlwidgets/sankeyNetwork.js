e1357d0ef1c1c99c1702a3991437e88b78e7a6bd
karkhaneei <karkhaneei@gmail.com>
2016-03-12 19:23:59 +0330

    correct tooltip for rtl languages

--- inst/htmlwidgets/sankeyNetwork.js
+++ inst/htmlwidgets/sankeyNetwork.js
@@ -17,7 +17,7 @@ HTMLWidgets.widget({
     },
 
     resize: function(el, width, height, instance) {
-        /*  handle resizing now through the viewBox
+        /*  handle resizing now through the viewBox 
         d3.select(el).select("svg")
             .attr("width", width)
             .attr("height", height + height * 0.05);
@@ -152,7 +152,7 @@ HTMLWidgets.widget({
 
         // note: u2192 is right-arrow
         link.append("title")
-            .text(function(d) { return d.source.name + " \u2192 " + d.target.name +
+            .text(function(d) { return  d.target.name + " \u2192 " + d.source.name +
                 "\n" + format(d.value) + " " + options.units; });
 
         node.append("rect")
